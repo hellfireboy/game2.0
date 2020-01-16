30daaa9a8c2c9a1ae118e9ad4aff21b0115ab40c
Ethan <emanon.me@foxmail.com>
2020-01-16 20:38:59 +0800

    for 1~100

--- D4/forin.py
+++ D4/forin.py
@@ -1,7 +1,13 @@
-# 1~100的求和
+# 1~100的各种求和
 
 sum = int(0)
 for a in range(100):  # a 初始值是 0
     sum += (a + 1)
 
-print("和为%d" % sum)
+print("1~100的和为%d" % sum)
+
+sum = int(0)
+for a in range(0, 101, 2):
+    sum += a
+
+print("1~100的偶数和为%d" % sum)
